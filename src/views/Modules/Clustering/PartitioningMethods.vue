949f37cf5b345f919119207cbd93257272e349b4
David Montaño <cuatro04sr@gmail.com>
2022-04-29 10:20:22 -0500

    fix: move commit 'download file in clustering tab' to show pca index

--- src/views/Modules/Clustering/PartitioningMethods.vue
+++ src/views/Modules/Clustering/PartitioningMethods.vue
@@ -507,6 +507,18 @@ BaseButton
         },
         //...
 
+        downloadFile(url, label){
+            axios.get(url, { responseType: 'blob' })
+                .then(response => {
+                    const blob = new Blob([response.data], { type: 'application/pdf' })
+                    const link = document.createElement('a')
+                    link.href = URL.createObjectURL(blob)
+                    link.download = label
+                    link.click()
+                    URL.revokeObjectURL(link.href)
+                }).catch(console.error)
+        },
+
         //Graph
         canvaConfig(){
             this.canvas = document.getElementById('myChart');
@@ -648,7 +660,7 @@ BaseButton
             }
 
             return respon;
-        }
+        },
         //...
 
     },
